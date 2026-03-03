8ff651e7c39f886879868eb1f06d35d3da8e0dc0
phunguyenthien-ncc <phu.nguyenthien@ncc.asia>
2026-03-03 13:59:06 +0700

    feat: theme base colors

---
+++ packages/app/theme/theme.ts
@@ -0,0 +1,77 @@
+export const themes = {
+  light: {
+    // ===== Surfaces =====
+    background: '#f8f6f6',
+    backgroundMuted: '$appBackgroundMuted',
+    surface: '$appSurface',
+
+    // ===== Text =====
+    color: '#000000',
+    colorMuted: '$appTextMuted',
+
+    // ===== Borders =====
+    borderColor: '$appBorder',
+    borderSubtle: '$appBorderSubtle',
+    appBorderSubtle: '#f1f5f9',
+
+    // ===== Brand =====
+    primary: '$appPrimary',
+    primaryHover: '$appPrimaryHover',
+    primaryActive: '$appPrimaryActive',
+    secondaryHover: '#dde6ee',
+
+    // ==== Card ======
+    backgroundCard: '#ffffff',
+
+    // ==== Toast ======
+    toastSuccessBg: '$green2',
+    toastSuccessBorder: '$green8',
+    toastSuccessText: '$green11',
+
+    toastErrorBg: '$red2',
+    toastErrorBorder: '$red8',
+    toastErrorText: '$red11',
+
+    toastInfoBg: '$blue2',
+    toastInfoBorder: '$blue8',
+    toastInfoText: '$blue11',
+  },
+
+  dark: {
+    // ===== Surfaces =====
+    background: '#101622',
+    backgroundMuted: '#0B1220',
+    surface: '#0F172A',
+    appSecondary: '#1e293b',
+
+    // ===== Text =====
+    appText: '#FFFFFF',
+    colorMuted: '#9CA3AF',
+
+    // ===== Borders =====
+    borderColor: '#1F2937',
+    appBorderSubtle: '$appPrimary',
+
+    // ===== Brand  =====
+    primary: '$appPrimary',
+    primaryHover: '$appPrimaryHover',
+    primaryActive: '$appPrimaryActive',
+    secondaryHover: '#2a344a',
+
+    // ==== Card ======
+    backgroundCard: '#171f2f',
+
+    // ==== Toast =====
+    toastSuccessBg: '#0f2e1c',
+    toastSuccessBorder: '$green8',
+    toastSuccessText: '$green3',
+
+    toastErrorBg: '#2a0f12',
+    toastErrorBorder: '$red8',
+    toastErrorText: '$red3',
+
+    toastInfoBg: '#0b2533',
+    toastInfoBorder: '$blue8',
+    toastInfoText: '$blue3',
+  },
+};
